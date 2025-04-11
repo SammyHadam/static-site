ca1b9e7e297d3ed32253b024e58e4bbff90c7e32
SammyHadam <sammyhadams@gmail.com>
2025-04-10 20:30:47 -0500

    added child node for htmlnode leafnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -6,6 +6,40 @@ class TestHTMLNode(unittest.TestCase):
     def test_none_for_self_props(self):
         htmlnode = HTMLNode("h1", "This is a heading", [], None)
         self.assertEqual(htmlnode.props_to_html(), "")
+    def test_values(self):
+        node = HTMLNode(
+            "div",
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.tag,
+            "div",
+        )
+        self.assertEqual(
+            node.value,
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.children,
+            None,
+        )
+        self.assertEqual(
+            node.props,
+            None,
+        )
+
+    def test_repr(self):
+        node = HTMLNode(
+            "p",
+            "What a strange world",
+            None,
+            {"class": "primary"},
+        )
+        self.assertEqual(
+            node.__repr__(),
+            "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
