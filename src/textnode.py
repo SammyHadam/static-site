027b54cc43d5597e84ecfd06eb058c357c132b7c
SammyHadam <sammyhadams@gmail.com>
2025-04-21 20:22:13 -0500

    finished textnode to htmlnode conversion function, need to write tests

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -21,3 +22,18 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    handlers = {
+        TextType.TEXT: lambda node: LeafNode(None, node.text),
+        TextType.BOLD: lambda node: LeafNode("b", node.text),
+        TextType.ITALIC: lambda node: LeafNode("i", node.text),
+        TextType.CODE: lambda node: LeafNode("code", node.text),
+        TextType.LINK: lambda node: LeafNode("a", node.text, {"href": node.url}),
+        TextType.IMAGE: lambda node: LeafNode("img", "", {"src": node.url, "alt": node.text})
+    }
+
+    if text_node.text_type in handlers:
+        return handlers[text_node.text_type](text_node)
+    else:
+        raise ValueError(f"Invalid text type: {text_node.text_type}")
