7f8d7e49b0ebdb2910ce7e9a678e18ef8327e66d
SammyHadam <sammyhadams@gmail.com>
2025-04-27 22:19:21 -0500

    finished splitting_nodes and its test

---
+++ src/splitting_nodes.py
@@ -0,0 +1,21 @@
+from textnode import TextNode, TextType
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    if not delimiter:
+        raise ValueError("Delimiter cannot be empty or None.")
+    new_node = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_node.append(node)
+            continue
+        parts = node.text.split(delimiter)
+        if len(parts) == 1:
+            new_node.append(node)
+            continue
+        if len(parts) % 2 == 0:
+            raise Exception("Invalid Markdown Syntax: Delimiter pair not found")
+        for i in range (len(parts)):
+            if i == 0 or i % 2 == 0:
+                new_node.append(TextNode(parts[i], TextType.TEXT))
+            else:
+                new_node.append(TextNode(parts[i], text_type))
+    return new_node
